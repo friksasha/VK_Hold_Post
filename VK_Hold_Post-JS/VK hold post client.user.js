87c2d620edec80ef2ed1bc23f09658107391ba77
Dmitry <Seedmanc@users.noreply.github.com>
2020-05-15 17:43:47 +0300

    Update VK hold post client.user.js
    рефакторинг
    Добавил корректное обновление ссылки при переключении между картинками (подсветка кнопок ещё не учитывается)
    Добавил работу при открытии из альбома

--- VK_Hold_Post-JS/VK hold post client.user.js
+++ VK_Hold_Post-JS/VK hold post client.user.js
@@ -4,7 +4,7 @@
 // @description		Формирует список ссылок с тегами для зарядки очереди
 // @author		Seedmanc
 // @include		/^https?://vk.com/photo-?\d+_\d+.*$/
-// @include		/^https?://vk.com/albums-?\d+\?z=photo-?\d+_\d+.*$/
+// @include		/^https?://vk.com/albums?-?\d+.*z=photo-?\d+_\d+.*$/
 // @require		https://code.jquery.com/jquery-3.5.1.slim.min.js
 // @grant		GM_addStyle
 // @grant		GM_setValue
@@ -18,16 +18,13 @@
 const Tags = 'kemono_friends kemurikusa cosplay translate@ hentatsu@ keifuku@';
 const Group = 'kemono_friends13th';
 
+
 GM_addStyle (`
 	#vkh-wrap {
 		position:  fixed;
 		display: flex;
 		top:       0px;
 		z-index:   500;
-	}
-	.vkh-button {
-		cursor: pointer;
-		flex: 1;
 		max-height: 45px;
 	}
 	.vkh-button.active {
@@ -49,52 +46,50 @@ GM_addStyle (`
 	}
 `);
 
-
 const Wrap = $('<div id="vkh-wrap"></div>').appendTo(document.body);
-const LinkList = $('<textarea id="link-list" wrap="off" cols="50" placeholder="Список ссылок с тегами"/>').appendTo(Wrap);
+const LinkList = $('<textarea id="link-list" wrap="off" cols="53" placeholder="Список ссылок с тегами"/>').appendTo(Wrap);
 const AddButton = $('<button style="font-weight: bold;" class="vkh-button">+</button>').appendTo(Wrap);
 const TagButtons = $('<div id="vkh-buttons"></div>').appendTo(Wrap);
-const DelButton = $('<button id="vkh-del-button" class="vkh-button">&ndash;</button>').appendTo(Wrap);
-const ClrButton = $('<button style="color: red;" class="vkh-button" title="Удалить всё">╳</button>').appendTo(Wrap);
+const DelButton = $('<button id="vkh-del-button">&ndash;</button>').appendTo(Wrap);
+const ClrButton = $('<button style="color: red;" title="Удалить всё">╳</button>').appendTo(Wrap);
 
 let links = {};
-let url = (new URLSearchParams(window.location.search)).get('z');
+let url;
 
+(function main() {
+    renderTagButtons();
+    updateUrl();
 
-main()
-
-function main() {
-	let tags = Tags.split(' ').map(t => '#' + t);
-	url = (url ?
-           ('vk.com/' + url.split('/')[0]) :
-           document.location.href.match(/.+vk.com\/photo-?\d+_\d+/)[0]
-    	).replace('https://', '');
-
-	tags.forEach(t => {
-		TagButtons.append(`<button class="vkh-button" data-tag="${t.replace('@', '@' + Group)}">${t}</button>`);
-	})
-
-	AddButton.click(tagClick);
-	TagButtons.click(tagClick);
+	Wrap.click(tagClick);
 	DelButton.click(delClick);
 	ClrButton.click(clrClick);
 
 	loadList();
+})();
+
+
+function renderTagButtons() {
+ 	let tags = Tags.split(' ').map(t => '#' + t);
+	tags.forEach(t => TagButtons.append(`<button class="vkh-button" style="flex: 1;" data-tag="${t.replace('@', '@' + Group)}">${t}</button>`));
 }
 
+function updateUrl() {
+    url = (new URLSearchParams(window.location.search)).get('z');
+	if (url)
+        url = 'vk.com/' + url.split('/')[0]
+    else
+        url = document.location.href.match(/.+(vk.com\/photo-?\d+_\d+)/)[1];
+}
 
 function loadList() {
 	let storedUrls = GM_listValues();
 
-	storedUrls.forEach(key => {
-		let storedValue = GM_getValue(key);
-		links[key] = {...storedValue, tags: new Set(storedValue.tags)};
-	});
-
+	storedUrls.forEach(key => links[key] = GM_getValue(key));
 	sync(true);
 }
 
 function delClick() {
+    updateUrl();
 	delete links[url];
 	GM_deleteValue(url);
 	sync(true);
@@ -110,20 +105,21 @@ function clrClick() {
 
 function tagClick(evt) {
 	let target = $(evt.target);
-	debugger;
 	if (!target.is('.vkh-button')) return;
 
+    updateUrl();
+
 	let tag = target.data('tag');
-	let entry = (links[url] || {link: url, tags: new Set()});
+	let entry = links[url] || {link: url, tags: []};
 
 	if (tag)
-		if (entry.tags.has(tag))
-			entry.tags.delete(tag)
+		if (entry.tags.includes(tag))
+			entry.tags.splice(entry.tags.indexOf(tag), 1)
 		else
-			entry.tags.add(tag);
+			entry.tags.push(tag);
 
 	links[url] = entry;
-	GM_setValue(url, {...entry, tags: [...entry.tags]});
+	GM_setValue(url, entry);
 
 	sync(true);
 }
@@ -131,10 +127,10 @@ function tagClick(evt) {
 function sync(out) {
 	if (out) {
 		LinkList.val(Object.values(links)
-			.map(({link, tags}) => `${link}  ${[...tags].join(' ')}`).join('\n'));
+			.map(({link, tags}) => `${link}  ${tags.join(' ')}`).join('\n'));
 
 		$('.vkh-button').removeClass('active');
 		if (links[url])
-			[...links[url].tags].forEach(tag => TagButtons.find(`.vkh-button[data-tag^="${tag}"`).addClass('active'));
+			links[url].tags.forEach(tag => TagButtons.find(`.vkh-button[data-tag^="${tag}"`).addClass('active'));
 	}
 }
